ef0b2221db723101cec567463ccdc0aac315e7b3
jasontujun <jasontujun@gmail.com>
2015-12-11 19:47:35 +0800

    fix navigation and left menu.

--- static/js/ui.js
+++ static/js/ui.js
@@ -2,9 +2,9 @@
 // init
 $(function () {
     var base =
-        '<header class="main-header"><%- include(\'views/partials/header.ejs\') %></header>' +
-        '<aside class="main-sidebar"><%- include(\'views/partials/left.ejs\', {user: user}) %></aside>' +
-        '<div class="content-wrapper" id="page-right-content"></div>' +
+        '<header class="main-header" style="position: fixed;"><%- include(\'views/partials/header.ejs\') %></header>' +
+        '<aside class="main-sidebar" style="position: fixed;"><%- include(\'views/partials/left.ejs\', {user: user}) %></aside>' +
+        '<div class="content-wrapper" id="page-right-content" style="padding-top: 50px"></div>' +
         '<footer class="main-footer"><%- include(\'views/partials/footer.ejs\') %></footer>' +
         '<%- include(\'views/partials/right.ejs\') %>';
 
